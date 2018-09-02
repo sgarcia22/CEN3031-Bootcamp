dda520852441cb5d30bf1f7f1b72e2a474ed6cee
Samantha Garcia <sgarcia22@ufl.edu>
2018-09-02 12:24:04 -0400

    Assignment One

--- server.js
+++ server.js
@@ -1,26 +1,54 @@
-var http = require('http'), 
-    fs = require('fs'), 
+//Samantha Garcia
+//CEN3031 Assignment #1
+
+var http = require('http'),
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
 /* Global variables */
 var listingData, server;
 
+/*
+  The request handler sends the listingData in the JSON format if a GET request
+  is sent to the '/listings' path. Otherwise, it sends a 404 error.
+ */
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
-
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+  if (parsedUrl.pathname != '/listings') {
+      response.statusCode = 404;
+      response.setHeader('Content-Type', 'text/plain');
+      response.write('Bad gateway error');
+  }
+  else {
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify(listingData));
+  }
+  response.end();
 };
 
+/*
+  This callback function saves the data in the listingData variable, and
+  then starts the server.
+ */
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+   //If error, return string of error
+   if (err) {
+     console.log(err);
+     throw err;
+   }
+
+   //Parse the json file into the listingData var
+   listingData = JSON.parse(data);
+
+   //Start the server
+   var server = http.createServer(requestHandler);
+
+   //The server is listening
+   server.listen(port, function () {
+     console.log('Received!');
+   });
+
+   console.log ("Server has started");
+
 });
--- server.test.js
+++ server.test.js
@@ -1,4 +1,4 @@
-var should = require('should'), 
+var should = require('should'),
     fs = require('fs'),
     request = require('request');
 
@@ -11,16 +11,16 @@ var listings;
 describe('UF Directory Server Unit Tests', function() {
 
   /*
-    This before hook loads the JSON data to the listings variable, so that we can compare 
-    the response to 'http://localhost:8080/listings' to the data we expect to recieve. 
+    This before hook loads the JSON data to the listings variable, so that we can compare
+    the response to 'http://localhost:8080/listings' to the data we expect to recieve.
    */
   before(function(done) {
     fs.readFile('listings.json', 'utf8', function(err, data) {
       listings = JSON.parse(data);
 
       /*
-        Calling done() will pass code execution to the next appropriate block of code. 
-        In this case, execution will pass to the first it() statement.  
+        Calling done() will pass code execution to the next appropriate block of code.
+        In this case, execution will pass to the first it() statement.
        */
       done();
     });
@@ -29,16 +29,16 @@ describe('UF Directory Server Unit Tests', function() {
   describe('Server responds to requests', function() {
     it('should respond', function(done) {
       /*
-        The request module allows us to make HTTP requests. This module could also be useful in 
-        making API calls to web services you make use of in your application, such as Google Maps. 
+        The request module allows us to make HTTP requests. This module could also be useful in
+        making API calls to web services you make use of in your application, such as Google Maps.
        */
       request.get('http://localhost:8080', function(error, response, body) {
         /*
           The 'should' module is an assertion library. Assertions allow us to compare the functions
-          that we are testing to the values we expect to recieve back. In this block, we expect that the 
-          server should respond to a request made. 
+          that we are testing to the values we expect to recieve back. In this block, we expect that the
+          server should respond to a request made.
 
-          Note in this unit test we are only testing the existence of a response, and are not concerned 
+          Note in this unit test we are only testing the existence of a response, and are not concerned
           with what is contained in the response.
          */
         should.not.exist(error);
