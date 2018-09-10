c0b6c6d2d15dcf8badd53ce579a0d4542939222c
Samantha Garcia <sgarcia22@ufl.edu>
2018-09-10 18:23:22 -0400

    Assignment 2

--- listingController.js
+++ listingController.js
@@ -1,14 +1,39 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.codec = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    //Check if the input and code or name of the building matches
+    $scope.valid = function (json) {
+           if ($scope.codec == undefined) return true;
+           return (json.code.toLowerCase().startsWith($scope.codec.toLowerCase()) ||
+                   json.name.toLowerCase().startsWith($scope.codec.toLowerCase()));
+    };
+
+    $scope.addListing = function() {
+      if ($scope.buildingCode == null || $scope.buildingName == null)
+        return;
+      var newListing =
+        {
+        "code" : $scope.buildingCode,
+        "name" : $scope.buildingName,
+        "coordinates" :
+          {
+          "latitude" : $scope.buildingLatitudeCoordinates,
+          "longitude" : $scope.buildingLongitudeCoordinates
+          },
+          "address" : $scope.buildingAddress
+        };
+        $scope.listings.push(newListing);
+    };
+
+    $scope.deleteListing = function(index) {
+      return $scope.listings.splice(index, 1);
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo =  $scope.listings[index];
+    };
   }
 ]);
